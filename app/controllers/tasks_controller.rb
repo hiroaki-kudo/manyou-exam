35ccf6683e07e9e9bc98ce6bfe8274b5e40c55c4
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-29 22:39:13 +0900

    ログインしてタスクを作成できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,6 +24,7 @@ class TasksController < ApplicationController
   end
   def create
     @task = Task.new(task_params)
+    @task.user_id = current_user.id
     if @task.save
       redirect_to tasks_path, notice: "タスクを作成しました！"
     else
