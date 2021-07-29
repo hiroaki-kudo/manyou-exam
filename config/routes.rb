00d3d663ea1829425d13606e9e2ca7970fcb2519
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-29 21:03:22 +0900

    log in画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
   root :to => 'tasks#index'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
