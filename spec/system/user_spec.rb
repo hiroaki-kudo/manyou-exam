b8589cf009ae992d7df07de855758178b749b3f5
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-31 15:25:04 +0900

    セッション機能テスト実装

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -22,31 +22,48 @@ RSpec.describe 'ユーザー管理機能', type: :system do
       it 'ログイン画面へ遷移する' do
         visit new_session_path
         click_on 'タスク一覧画面へ'
-        binding.pry
         expect(page).to have_content 'Log in'
       end
     end
   end
 
-  # describe 'セッション機能に関するテスト' do
-  #   context 'ユーザーがログインした場合' do
-  #     it 'ユーザーの詳細画面へ遷移する' do
-  #       visit new_session_path
-  #       fill_in 'user[email]', with: 'naitou@naitou.com'
-  #       fill_in 'user[password]', with: 'naitounaitou'
-  #       click_button 'commit'
-  #       expect(page).to have_content 'naitou'
-  #       expect(page).to have_content 'naitou@naitou.com'
-  #     end
-  #   end
-  #   context 'ユーザがログインせずにタスク一覧画面へ飛ぼうとした場合' do
-  #     it 'ログイン画面へ遷移する' do
-  #       visit new_session_path
-  #       click_on 'タスク一覧画面へ'
-  #       binding.pry
-  #       expect(page).to have_content 'Log in'
-  #     end
-  #   end
-  # end
+  describe 'セッション機能に関するテスト' do
+    context 'ユーザーがログインした場合' do
+      it 'ログインできていること' do
+        visit new_session_path
+        fill_in 'Email', with: 'kudou@kudou.com'
+        fill_in 'Password', with: 'kudoukudou'
+        click_button 'commit'
+        expect(page).to have_content 'ログイン中'
+      end
+    end
+    context 'ユーザーがログインした場合' do
+      it '自分のマイページへ飛ぶ' do
+        visit new_session_path
+        fill_in 'Email', with: 'satou@satou.com'
+        fill_in 'Password', with: 'satousatou'
+        click_button 'commit'
+        expect(page).to have_content 'satou'
+        expect(page).to have_content 'satou@satou.com'
+      end
+    end
+    context '一般ユーザーが他人の詳細画面に飛んだ場合' do
+      it 'タスク一覧画面に遷移' do
+
+      end
+    end
+    context 'ログアウトを押した場合' do
+      it 'ログアウトできる' do
+        visit new_session_path
+        fill_in 'Email', with: 'satou@satou.com'
+        fill_in 'Password', with: 'satousatou'
+        click_button 'commit'
+        click_on "Logout"
+        expect(page).to have_content 'ログアウトしました'
+      end
+    end
+  end
+
+
 
 end
