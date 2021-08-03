6aaf63a65bde2e9d55c4357437f11fe07a67d3eb
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-03 20:46:59 +0900

    タスク詳細画面で選択したラベル一覧を表示できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,22 @@
                admin: "true"
                )
 
-  5.times do |i|
-    Label.create!(title: "sample#{i + 1}")
+  9.times do |i|
+  User.create!(name: "佐藤#{i + 1}",
+                email: "satou#{i + 1}@satou.com",
+                password: "satousatou",
+                admin: "false"
+                )
   end
+
+  10.times do |i|
+  Task.create!(name: "勉強#{i + 1}",
+               detail: "プログラミング#{i + 1}",
+               end_date: "002025-07-0#{i + 1}",
+               status: "完了",
+               rank: "低",
+               )
+  end
+
+  10.times do |i|
+    Label.create!(title: "sample#{i + 1}")
