f4e997bde2674e5260874356c3bdb8ce3791d92c
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-04 00:56:23 +0900

    シードデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-
-  User.create!(name: "kamisama",
+  user = User.create!(name: "kamisama",
                email: "kamisama@kamisama.com",
                password: "kamisamakamisama",
                admin: "true"
@@ -20,15 +12,16 @@
                 admin: "false"
                 )
   end
-  #
-  # 10.times do |i|
-  # Task.create!(name: "勉強#{i + 1}",
-  #              detail: "プログラミング#{i + 1}",
-  #              end_date: "002025-07-0#{i + 1}",
-  #              status: "完了",
-  #              rank: "低"
-  #              )
-  # end
+
+  10.times do |i|
+  Task.create!(name: "勉強#{i + 1}",
+               detail: "プログラミング#{i + 1}",
+               end_date: "2025-07-01",
+               status: "完了",
+               rank: "低",
+               user: user
+               )
+  end
 
   10.times do |i|
     Label.create!(title: "sample#{i + 1}")
