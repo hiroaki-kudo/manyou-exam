cea9be28c5a2152305a765913d099c0c5a744716
kudo_hiroaki <pikoniconico26@gmail.com>
2021-08-04 15:14:16 +0900

    ラベルテスト修正

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -57,6 +57,35 @@ RSpec.describe 'タスク管理機能', type: :system do
      end
   end
 
+  describe '編集機能作成' do
+     context '任意のタスクを編集した場合' do
+       it '任意のタスク（ラベルも含んで）編集できている' do
+         visit new_session_path
+         fill_in 'Email', with: 'kudou@kudou.com'
+         fill_in 'Password', with: 'kudoukudou'
+         click_button 'commit'
+         click_on 'タスク一覧画面へ'
+         click_on 'タスクの新規作成'
+         fill_in 'task[name]', with: '勉強'
+         fill_in 'task[detail]', with: 'プログラミング'
+         fill_in 'task[end_date]', with: '002025-07-01'
+         select '未着手'
+         select '低'
+         check "sample1"
+         click_button 'commit'
+         task = Task.find_by(name: '勉強')
+         click_link '編集', href: edit_task_path(task.id)
+         check "sample2"
+         click_button 'commit'
+         binding.pry
+         click_link '詳細', href: task_path(task.id)
+         binding.pry
+         expect(page).to have_content 'sample1'
+         expect(page).to have_content 'sample2'
+       end
+     end
+  end
+
   describe '検索機能' do
     context 'ラベルを１つ選択して検索をした場合' do
       it "そのラベルを含むタスクで絞り込まれる" do
